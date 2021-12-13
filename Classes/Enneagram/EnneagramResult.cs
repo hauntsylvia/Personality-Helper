02db886f3a285f3f6454f958cd78e2c5d5dc97ad
Izabella Williams <iza.yy10@gmail.com>
2021-12-13 12:59:17 -0500

    a

---
+++ Classes/Enneagram/EnneagramResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personality_helper.Classes.Enneagram
+{
+    public class EnneagramResult
+    {
+        public EnneagramResult(IEnneagram dominantType, IEnneagram wing)
+        {
+            this._dominantType = dominantType;
+            int magnitude = Math.Abs(dominantType.representativeNumber - wing.representativeNumber);
+            if (magnitude > 1)
+                throw new ArgumentException($"{nameof(wing)} can not be more than 1 step away from {nameof(dominantType)}", nameof(wing));
+            this._wingType = wing;
+        }
+        private readonly IEnneagram _dominantType;
+        public IEnneagram dominantType { get => _dominantType; }
+        private readonly IEnneagram _wingType;
+        public IEnneagram wingType { get => _wingType; }
+
+    }
+}
--- Classes/FullPersonality.cs
+++ Classes/FullPersonality.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using personality_helper.Classes.Enneagram;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,15 @@ namespace personality_helper.Classes
 {
     public class FullPersonality
     {
-        public FullPersonality(MyersBriggsResult mbti)
+        public FullPersonality(MyersBriggsResult mbti, EnneagramResult enneagram)
         {
             this._mbti = mbti;
+            this._enneagram = enneagram;
         }
         private MyersBriggsResult _mbti;
         public MyersBriggsResult mbti { get => _mbti; set => _mbti = value ?? throw new ArgumentNullException(nameof(value)); }
+
+        private EnneagramResult _enneagram;
+        public EnneagramResult enneagram { get => _enneagram; set => _enneagram = value ?? throw new ArgumentNullException(nameof(value)); }
     }
 }
